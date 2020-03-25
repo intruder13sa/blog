586d40fc9a1a3763f4231e85317f75c1394510a8
Stanislav <intruder13sa@gmail.com>
2020-03-25 23:16:20 +0300

    Lesson 4

--- app.js
+++ app.js
@@ -3,16 +3,26 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-const arr = ["hello", "world", "test"];
+const Post = require("./models/post");
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => res.render("index", { arr: arr }));
+app.get("/", (req, res) => {
+  Post.find({}).then(posts => {
+    res.render("index", { posts: posts });
+  });
+});
 
 app.get("/create", (req, res) => res.render("create"));
 app.post("/create", (req, res) => {
-  arr.push(req.body.text);
+  const { title, body } = req.body;
+
+  Post.create({
+    title: title,
+    body: body
+  }).then(post => console.log(post.id));
+
   res.redirect("/");
 });
 
